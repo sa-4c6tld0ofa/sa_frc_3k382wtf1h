8924407a842e2da092a99be992591ac89e758d29
daivieth <daiviet.huynh@gmail.com>
2019-01-25 16:35:12 +0700

    dev

---
+++ specol/sa_hist_forecast - s.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\R\R-3.5.1\bin\x64\Rscript.exe" "specific_collect_data.R"
+"specific_insert_db_price_data.py"
+"C:\Program Files\R\R-3.5.1\bin\x64\Rscript.exe" "specific_hist_forecast_arima_instr.R"
