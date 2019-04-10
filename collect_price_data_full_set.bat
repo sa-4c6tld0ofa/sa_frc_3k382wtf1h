18820508d541dabe89e9f02454cb16a6e9e901ac
daivieth <daiviet.huynh@gmail.com>
2019-04-10 13:01:01 +0700

    dev

--- collect_price_data_full_set.bat
+++ collect_price_data_full_set.bat
@@ -8,5 +8,5 @@ SET _R_SCRIPT_EXE="C:\Program Files\R\%R_VER%\bin\x64\Rscript.exe"
 SET _PIP_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\Scripts\pip.exe"
 SET _PY_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\python.exe"
 
-START "" %_PY_EXE% "%SA_DATA_DIR%sa_data_collection\core\collection_instr_fulldata_full_set.py"
+%_PY_EXE% "%SA_DATA_DIR%sa_data_collection\core\collection_instr_fulldata_full_set.py"
 exit
