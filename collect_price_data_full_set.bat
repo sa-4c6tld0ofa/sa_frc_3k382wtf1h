f11e1bcffd300ca43e771caedb9c945d4591ae72
daivieth <daiviet.huynh@gmail.com>
2019-04-10 13:04:07 +0700

    dev

--- collect_price_data_full_set.bat
+++ collect_price_data_full_set.bat
@@ -3,7 +3,7 @@ SET R_VER=R-3.5.1
 SET PY_VER=Python37-32
 REM ############################################################################
 
-SET SA_DATA_DIR=%~dp0
+SET SA_DATA_DIR=c:\smartalpha\
 SET _R_SCRIPT_EXE="C:\Program Files\R\%R_VER%\bin\x64\Rscript.exe"
 SET _PIP_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\Scripts\pip.exe"
 SET _PY_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\python.exe"
