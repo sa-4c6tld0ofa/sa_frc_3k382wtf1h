a5fb97c2b30b14fbb4b228f1d601fe5718398687
daivieth <daiviet.huynh@gmail.com>
2019-04-15 13:42:14 +0700

    dev

--- output_prediction.py
+++ output_prediction.py
@@ -22,8 +22,8 @@ from sa_access import *
 access_obj = sa_db_access()
 
 sys.path.append(os.path.abspath(sett.get_path_core() ))
-from get_instr_perf_summ import *
-from sa_numeric import *
+from ta_main_update_data import *
+from get_frc_pnl import *
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
@@ -65,92 +65,6 @@ def clear_trades(s):
         connection.close()
 
     except Exception as e: print(e)
-
-
-
-def get_forecast_pnl(s,uid,nd):
-
-
-    import pymysql.cursors
-    connection = pymysql.connect(host=db_srv,
-                                 user=db_usr,
-                                 password=db_pwd,
-                                 db=db_name,
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    td = datetime.datetime.now()
-    i = 0
-    wdb = 7
-
-    while i <= nd:
-
-        j = nd - i
-        k = (nd - i) + wdb
-        pd = datetime.datetime.now() - timedelta(days=k)
-        sd = datetime.datetime.now() - timedelta(days=j)
-        pd_str = pd.strftime("%Y%m%d")
-        sd_str = sd.strftime("%Y%m%d")
-
-        signal = ''
-        p_price_close = 0
-        p_target_price = 0
-        pnl = 0
-        pnl_long = 999
-        pnl_short = 999
-
-        print(s +": "+ sd_str +": "+ os.path.basename(__file__) )
-
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT price_close, target_price FROM price_instruments_data WHERE symbol ='"+s+"' AND date = "+ pd_str
-        cr.execute(sql)
-        rs = cr.fetchall()
-        for row in rs:
-            p_price_close = row[0]
-            p_target_price = row[1]
-        cr.close()
-
-        if (p_price_close > 0 and p_target_price > 0 ):
-
-            if p_price_close < p_target_price:
-                signal = "b"
-            else:
-                signal = "s"
-
-            id = 0
-            s_pnl = 0
-            s_pnl_long = 0
-            s_pnl_short = 0
-            s_price_close = 0
-            cr = connection.cursor(pymysql.cursors.SSCursor)
-            sql = "SELECT id, price_close, pnl, pnl_long, pnl_short FROM price_instruments_data WHERE symbol ='"+s+"' AND date = "+ sd_str
-            print(sql)
-            cr.execute(sql)
-            rs = cr.fetchall()
-            for row in rs:
-                id = row[0]
-                s_price_close = row[1]
-                s_pnl = row[2]
-                s_pnl_long = row[3]
-                s_pnl_short = row[4]
-            cr.close()
-
-            if signal == "b":
-                pnl = s_price_close - p_price_close
-                pnl_long = pnl
-            if signal == "s":
-                pnl = p_price_close - s_price_close
-                pnl_short = pnl
-            cr = connection.cursor(pymysql.cursors.SSCursor)
-            sql = "UPDATE price_instruments_data SET pnl = " + str(pnl) + ", pnl_long = " + str(pnl_long) + ", pnl_short = " + str(pnl_short) + " WHERE id = " + str(id)
-            print(sql)
-            try:
-                cr.execute(sql)
-                connection.commit()
-                cr.close()
-            except Exception as e: print("get_forecast_pnl() " + str(e) )
-        i += 1
-    connection.close()
 
 def get_instr_decimal_places(s):
     r = 5
@@ -236,7 +150,7 @@ def compute_target_price(uid,force_full_update):
             cr.execute(sql); connection.commit()
             clear_chart_data(symbol)
             clear_trades(symbol)
-            get_forecast_pnl(symbol,uid,nd)
+            get_update_instr_data(1,True,symbol)
         else:
             sql = "UPDATE price_instruments_data SET price_instruments_data.target_price = FORMAT(" + selected_model_column + ","+ str( get_instr_decimal_places(symbol) ) +") WHERE price_instruments_data.id = " + str(price_id)
             cr.execute(sql); connection.commit()
