8abbb5c13325cebb36c59cec484ae31d74e669f7
daivieth <daiviet.huynh@gmail.com>
2019-04-19 08:33:56 +0700

    dev

--- get_forecast_data.bat
+++ get_forecast_data.bat
@@ -14,5 +14,5 @@ ping 127.0.0.1 -n 120
 %_R_SCRIPT_EXE% "%SA_DATA_DIR%r_forecast\forecast_arima_dsc.R"
 
 REM ### Output the prediction model
-%_PY_EXE% "%SA_DATA_DIR%output_prediction.py"
+%_PY_EXE% "%SA_DATA_DIR%get_prediction_model_not_fullset.py"
 exit
