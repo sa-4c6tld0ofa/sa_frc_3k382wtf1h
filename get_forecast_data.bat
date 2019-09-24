2884394e858e648f5ffe9be9d5343fbf68afa09d
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-24 11:06:01 +0700

    dev

--- get_forecast_data.bat
+++ get_forecast_data.bat
@@ -5,11 +5,13 @@ REM ############################################################################
 
 SET SA_DATA_DIR=%~dp0
 SET _R_SCRIPT_EXE="C:\Program Files\R\%R_VER%\bin\x64\Rscript.exe"
+SET _R_SCRIPT_EXE_FILENAME="Rscript.exe"
 SET _PIP_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\Scripts\pip.exe"
 SET _PY_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\python.exe"
 
 REM ### Get Forecast
 %_R_SCRIPT_EXE% "%SA_DATA_DIR%r_forecast\forecast_arima_asc.R"
+TASKKILL /F /IM %_R_SCRIPT_EXE_FILENAME%
 %_R_SCRIPT_EXE% "%SA_DATA_DIR%r_forecast\forecast_arima_dsc.R"
 
 REM ### Output the prediction model
